9fb30096d4a252281836675a16a4560bf9631cd9
Arthur Violy <arthur@violy.net>
2019-11-15 12:39:43 +0100

    player

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,47 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import {Component} from "react"
+import logoSVG from "../images/logo-radio404-black-and-white.svg"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+    };
+    this.PlayPause = this.PlayPause.bind(this);
+  }
+
+  PlayPause(){
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+    console.log('PlayPause',currentState);
+  }
+
+  render(){
+    return (<header className={'site-header'}>
+      <div>
+        <h1 className={'site-header__logo'}>
+          <Link to="/">
+            <img src={logoSVG} alt={`radio404`}/>
+            <span>{`radio404`}</span>
+          </Link>
+        </h1>
+        <div className="player">
+          <div className="player__buttons">
+            <button type="button" className={(this.state.active ? 'playing ': '')+ 'play-button'} onClick={this.PlayPause} />
+          </div>
+          <div className="now-playing">
+            <div className={`now-playing__track`}>New Heights (Visions of Aisha Malik)</div>
+            <div className={`now-playing__artist`}>Kamaal Williams, Darkhouse Family</div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )}
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,9 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "../fonts/Inter/inter.css"
+import "../fonts/icomoon/style.css"
+import "../scss/layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,20 +27,10 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <div className="site">
+        <Header siteTitle={data.site.siteMetadata.title} />
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
     </>
